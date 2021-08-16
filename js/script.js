c7a0ea6dc17dc97b81bc05a9842382f7be1d8767
Yaroslav <example@gmail.com>
2021-08-16 19:11:30 +0300

    Implemented a timer at the bottom of the website

--- js/script.js
+++ js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
+
+    //Tabs
+
     const tabs = document.querySelectorAll('.tabheader__item'),
           tabsContent = document.querySelectorAll('.tabcontent'),
           tabsParent = document.querySelector('.tabheader__items');
@@ -36,4 +39,57 @@ window.addEventListener('DOMContentLoaded', () => {
             });
         }
     });
+
+    //Timer
+    let deadline = '2021-08-30';
+
+    function getTimeRemaining(endtime) {
+        const diffTime = Date.parse(endtime) - new Date(),
+              days = Math.floor(diffTime / (1000 * 3600 * 24)),
+              hours = Math.floor((diffTime / (1000 * 3600)) % 24),
+              minutes = Math.floor((diffTime / 1000 / 60) % 60),
+              seconds = Math.floor((diffTime / 1000) % 60);
+
+        return {
+            total: diffTime,
+            days: days,
+            hours: hours,
+            minutes: minutes,
+            seconds: seconds,
+        };
+    }
+
+    function setZero(number) {
+        if (number >= 0 && number < 10) {
+            return `0${number}`;
+        } else {
+            return number;
+        }
+    }
+
+    function setClock(selector, endtime) {
+        const timer = document.querySelector(selector),
+              days = timer.querySelector('#days'),
+              hours = timer.querySelector('#hours'),
+              minutes = timer.querySelector('#minutes'),
+              seconds = timer.querySelector('#seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+
+        updateClock();
+
+        function updateClock() {
+            let diffTime = getTimeRemaining(endtime);
+
+            days.textContent = setZero(diffTime.days);
+            hours.textContent = setZero(diffTime.hours);
+            minutes.textContent = setZero(diffTime.minutes);
+            seconds.textContent = setZero(diffTime.seconds);
+
+            if (diffTime <= 0) {
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('.timer', deadline);
 });
