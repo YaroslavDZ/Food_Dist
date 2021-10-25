106f4f4566c457052c0df154ddc82a9e91dd238d
Yaroslav <dzisyuk.yari@gmail.com>
2021-10-25 13:12:55 +0300

    module

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 window.addEventListener('DOMContentLoaded', () => {
-<<<<<<< HEAD
     const tabs = require('../modules/tabs.js'),
           modal = require('../modules/modal.js'),
           slider = require('../modules/slider.js'),
@@ -13,43 +12,4 @@ window.addEventListener('DOMContentLoaded', () => {
     modal();
     slider();
     cards();
-=======
-    const tabs = document.querySelectorAll('.tabheader__item'),
-          tabsContent = document.querySelectorAll('.tabcontent'),
-          tabsParent = document.querySelector('.tabheader__items');
-    
-    function hideTabContent() {
-
-        tabsContent.forEach(item => {
-            item.classList.add('hide');
-            item.classList.remove('show');
-        });
-
-        tabs.forEach(item => {
-            item.classList.remove('tabheader__item_active');
-        });
-    }
-
-    function showTabContent(i = 0) {
-        tabsContent[i].classList.add('show', 'fade');
-        tabsContent[i].classList.remove('hide');
-        tabs[i].classList.add('tabheader__item_active');
-    }
-
-    hideTabContent();
-    showTabContent();
-
-    tabsParent.addEventListener('click', (event) => {
-        const target = event.target;
-
-        if(target && target.classList.contains('tabheader__item')) {
-            tabs.forEach( (item, i) => {
-               if(target == item) {
-                   hideTabContent();
-                   showTabContent(i);
-               } 
-            });
-        }
-    });
->>>>>>> b6ba2e6... I've set up tabs on the site, also added classes 'show', 'hide', 'fade' in the css filein order to the tabs display in the right way. Besides I've changed index.html removed comments
 });
